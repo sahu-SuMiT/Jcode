c7160af41a879c758e0d800ac8adb6ec29629516
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2025-05-22 16:22:01 +0530

    Update kadanes.java
    added collected rainwater

--- kadanes.java
+++ kadanes.java
@@ -12,3 +12,21 @@
         }
         System.out.println("Max Sum : "+ maxSum);
     }
+
+// collect rainwater
+int collectedWater = 0;
+        int[] height = {4,2,0,6,3,2,5};
+        int[] maxHeightLeft = new int[height.length];
+        int[] maxHeightRight = new int[height.length]; 
+        maxHeightLeft[0] = height[0]; 
+        maxHeightRight[height.length-1] = height[height.length-1];
+        for(int i = 1; i<height.length; i++){
+            maxHeightLeft[i] = Math.max(height[i], maxHeightLeft[i-1]);
+        }
+        for(int i = height.length-2; i>=0; i--){
+            maxHeightRight[i] = Math.max(height[i], maxHeightRight[i+1]);
+        }
+        for(int i = 0; i < height.length; i++){
+            collectedWater += Math.min(maxHeightLeft[i], maxHeightRight[i]) - height[i];
+        }
+        System.out.println(collectedWater);
