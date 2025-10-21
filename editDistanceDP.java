7c625678dbcb58303f85d48c8f48fbda913ae84c
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2025-10-21 20:53:52 +0530

    Create editDistanceDP.java

---
+++ editDistanceDP.java
@@ -0,0 +1,19 @@
+    public static int minOperation(String s1, String s2, int n, int m, int[][]dp){
+        //initialise dp
+        for(int i=0;i<dp.length;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
